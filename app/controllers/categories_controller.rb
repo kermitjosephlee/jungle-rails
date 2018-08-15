3ab123e4bb857211cba6497b0ab5038a00159697
Kermit Joseph Lee <kermitjosephlee@gmail.com>
2018-08-15 18:58:38 -0400

    got categories to work, but categories -id is broken

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,14 @@
 class CategoriesController < ApplicationController
 
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
