720ebaf10b9588640499663dced69ea4f3b36129
Kermit Joseph Lee <kermitjosephlee@gmail.com>
2018-08-17 12:52:10 -0400

    added in validations for ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,6 @@
 class Rating < ActiveRecord::Base
+  validates :product, presence: true
+  validates :user, presence: true
+  validates :description, presence: true
+  validates :rating, presence: true
 end
